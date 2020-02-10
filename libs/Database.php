ba729c9fddf2db890ec0b52a809667b3912fa803
Alejandro Morales Conejo <43568559+AlejandroMoralesConejo@users.noreply.github.com>
2020-02-10 21:17:17 +0100

    Add files via upload

---
+++ libs/Database.php
@@ -0,0 +1,23 @@
+<?php
+
+    class Database
+    {
+        private $conexion;
+        private $resultado;
+
+        public function __construct($host = "localhost", $dbName = "misseries", $user = "root", $pass = "")
+        {
+            $this->conexion = new PDO("mysql:host=".$host.";dbname=".$dbName.";charset=utf8", $user, $pass)
+                        or die("Error en la conexión con la base de datos");
+        }
+
+        public function __destruct()
+        {
+            $this->conexion = null;
+        }
+
+        public function query($sql)
+        {
+            $this->resultado = $this->conexion->query($sql);
+        }
+    }
